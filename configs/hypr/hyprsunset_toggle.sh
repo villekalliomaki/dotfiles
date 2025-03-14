f2515752f3c6f021625e3488d4a1a86a5777fcd4
Ville Kalliomäki <i@v-k.fi>
2025-03-14 17:46:58 +0200

    Basic playbooks

---
+++ configs/hypr/hyprsunset_toggle.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+if pgrep hyprsunset >/dev/null; then
+    if [ "$1" = "toggle" ]; then
+        pkill hyprsunset
+        notify-send -t 700 "Hyprsunset stopped"
+        echo "Hyprsunset toggle: stopped"
+        pkill -SIGRTMIN+10 waybar
+    else
+        echo "󱩌"
+    fi
+else
+    if [ "$1" = "toggle" ]; then
+        hyprsunset &
+        notify-send -t 700 "Hyprsunset started"
+        echo "Hyprsunset toggle: started"
+        pkill -SIGRTMIN+10 waybar
+    else
+        echo "󰹏"
+    fi
+fi
